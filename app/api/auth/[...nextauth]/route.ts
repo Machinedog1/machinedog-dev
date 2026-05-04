6e6a713ea87d530ce87304324b385be1960acbcb
unknown <tom@machinedog.com>
2026-05-03 23:25:18 -0500

    remove prisma from auth (stabilize)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,7 @@
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 const handler = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID!,
