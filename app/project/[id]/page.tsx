e7258079c0e72bfce183f5f6d229f83cc391b88c
unknown <tom@machinedog.com>
2026-05-04 09:26:18 -0500

    fix params id passing

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,6 +1,5 @@
 import ProjectClient from "./ProjectClient";
 
 export default function Page({ params }: { params: { id: string } }) {
-  console.log("SERVER PARAMS:", params);
   return <ProjectClient id={params.id} />;
 }
